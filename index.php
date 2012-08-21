1fa9fbdd1dedac1830e5ad21c83d2cbaf1d8b6a1
3sidedcoin <tejas@3sided.co.in>
2012-08-21 15:38:10 +0530

    added index file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html> 
+<html manifest="country.appcache">
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<meta name="description" content="html5 offlineapp with javascript and php"/>
+<meta name="author" content="3sided coin"/>
+<head>
+  <title>offline app with html5, javascript and php</title>  
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+  
+</head>
+<body>
+	index page
+	
+</body>
+</html>
